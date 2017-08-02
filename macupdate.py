38a5f53d771926d6d5b6684e37993567ba9ad1bb
eLVee1991 <elvee@protonmail.com>
2017-08-02 19:26:18 +0200

    corrected spacces

--- macupdate.py
+++ macupdate.py
@@ -37,6 +37,8 @@ ___  ___  ___  _____    _____ _____
 	print("Created by eLVee")
 	print("-"*60)
 	print("")
+  print("see my github account:")
+  print("https://github.com/eLVee1991/MacUpdate")
 
 def Main():
 	usage = """
