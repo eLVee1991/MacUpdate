6563641e94e49fbc6cc22fc1076f6cfbfd245298
eLVee1991 <elvee@protonmail.com>
2017-10-09 18:48:21 +0200

    Created install and update part

--- install-run.sh
+++ install-run.sh
@@ -7,16 +7,21 @@
 
 echo "-------------------------------------------------------------"
 echo ""
-echo "Retrieving macupdate.py from Github please wait"
+echo "Retrieving macupdate.py from Github. Please wait"
 echo ""
 echo "-------------------------------------------------------------"
-cd $HOME && wget https://raw.githubusercontent.com/eLVee1991/MacUpdate/master/macupdate.py
+# cd is set to the homedir, removing (if there is an) old version of macupdate.py and then cloning the new one into the homedir
+cd $HOME && rm -rf macupdate.py && wget https://raw.githubusercontent.com/eLVee1991/MacUpdate/master/macupdate.py
 echo "-------------------------------------------------------------"
 echo "-------------------------------------------------------------"
 echo ""
+echo "Running macupdate.py -i to install the dependencies on the mac"
+echo ""
+echo "-------------------------------------------------------------"
+python3 macupdate.py -i
+echo "-------------------------------------------------------------"
 echo "Running macupdate.py -u to update the mac and App Store apps"
 echo ""
 echo "-------------------------------------------------------------"
-#Change this to 'python3 macupdate.py -i' if you want to install the script. After this you van run the update script. with -u
 python3 macupdate.py -u
 echo "-------------------------------------------------------------"
