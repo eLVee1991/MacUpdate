af8b4d16f14bb2217856f61280b57b0108e4a581
eLVee1991 <elvee@protonmail.com>
2017-10-16 23:01:17 +0200

    Updated to version 1.4
    Created color class instead of using single and double hashes for marking code. Only using pexpect for running commands now. Showing output to the user.

--- macupdate.py
+++ macupdate.py
@@ -5,6 +5,33 @@
 import subprocess
 import sys
 import getpass
+import pexpect
+
+class colors:
+	HEADER = '\033[95m'
+	BLUE = '\033[94m'
+	GREEN = '\033[92m'
+	WARNING = '\033[93m'
+	RED = '\033[91m'
+	END = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+
+def message(state, msg):
+	if state == "succes":
+		print(colors.GREEN + msg + colors.END)
+	elif state == "warning":
+		print(colors.WARNING + msg + colors.END)
+	elif state == "header":
+		print(colors.HEADER + msg + colors.END)
+	elif state == "underline":
+		print(colors.UNDERLINE + msg + colors.END)
+	elif state == "bold":
+		print(colors.BOLD + msg + colors.END)
+	elif state == "info":
+		print(colors.RED + msg + colors.END)
+	else:
+		print(msg)
 
 def Main():
 	usage = """
@@ -18,7 +45,6 @@ optional arguments:
   -ip    Setup a static ip on a mac.
 """
 
-	Text()
 	argument = sys.argv
 	try:
 		if argument[1] == "-i":
@@ -37,123 +63,120 @@ optional arguments:
 			while True:
 				ssh = input("Do you want to enable/disable? e/d: ")
 				if ssh == "e":
-					subprocess.Popen("sudo systemsetup -f -setremotelogin on", shell=True)
-					print("SSh was enabled")
-					print("-"*60)
+					command = pexpect.spawnu("sudo systemsetup -f -setremotelogin on")
+					command.expect("Password:")
+					command.sendline(password)
+					message("succes", "[+] SSH was enabled")
 					print("")
 					break
 				elif ssh == "d":
-					subprocess.Popen("sudo systemsetup -f -setremotelogon off", shell=True)
-					print("SSH was disabled.")
-					print("-"*60)
+					command = pexpect.spawnu("sudo systemsetup -f -setremotelogon off")
+					command.expect("Password:")
+					command.sendline(password)
+					message("succes", "[+] SSH was disabled")
 					print("")
 					break
 				else:
-					print("Error: wrong input. Please enter e/d")
+					message("warning", "[+] Wrong input. Please enter 'e'or 'd'")
 		elif argument[1] == "-ip":
 			ip = input("Please enter the ip you want to give this mac: ")
-			subprocess.Popen("sudo ipconfig set en1 INFORM "+ip, shell=True)
-			print("Changed the ip to: "+ip)
+			command = pexpect.spawnu("sudo ipconfig set en1 INFORM "+ip)
+			command.expect("Password:")
+			command.sendline(password)
+			message("succes", "[+] Changed the ip to: "+ip)
 			print("-"*60)
 			print("")
 		elif argument[1] is not "-i" or "-u" or "-h" or "-s" or "-ip":
-			print(usage)
+			message("warning", usage)
 			exit()
 
 	except IndexError:
 		print("-"*60)
-		print("Error: No 2nd argument added. See the usage below:")
+		message("error", "[+] No 2nd argument added. See the usage below:")
 		print("-"*60)
 		print("")
 		print(usage)
 		exit()
 
 def Mac_Update():
-	while True:
-		try:
-			import pexpect
-			#Run the command in the terminal before running the script.
-			print("Please enter your password here to be able to install/update: ")
-			password = getpass.getpass()
-			print("-"*60)
-			command_1 = pexpect.spawnu('sudo softwareupdate -ia --verbose')
-			#command_1.interact()
-			print("Running script for updating the mac from commandline.")
-			command_1.expect('Password:')
-			print("Asking for password. Passing it now.")
-			command_1.sendline(password)
-			command_1.sendline()
-			print("Done")
-			print("Updating now.")
-			#command_1.close()
-			print("Done updating Mac OS Software")
-			print("-"*60)
-			print("")
-			break
-		except ModuleNotFoundError:
-			while True:
-				print("The python module 'pexpect' is needed for this script to work.")
-				module = input("Do you want to install pexpect now? y/n: ")
-				if module == "y":
-					subprocess.Popen("pip3 install pexpect", shell=True)
-					print("Done installing pexpect")
-					print("Please run the script again.")
-					print("-"*60)
-					print("")
-					break
-				elif module == "n":
-					print("Closing the script.")
-					print("-"*60)
-					print("")
-					break
-				else:
-					print("-"*60)
-					print("Error: Please answer with y/n")
-					print("-"*60)
-					print("")
+	#Run the command in the terminal before running the script.
+	command_1 = pexpect.spawnu('sudo softwareupdate -ia --verbose')
+	message("succes", "[+] Updating your mac. Please wait.")
+	command_1.expect('Password:')
+	command_1.sendline(password)
+	command_1.sendline()
+	command_1.interact()
+	command_1.close()
+	message("succes", "[+] Done updating Mac OS Software")
+	print("")
 
 def Text():
-	print("""
-######################################################
-#						     #
-#	     MAC OS AND APPSTORE UPDATER	     #
-#		       				     #
-######################################################
+	message("info", """
+
+	 MM    MM   AAA    CCCCC    OOOOO   SSSSS  
+	 MMM  MMM  AAAAA  CC    C  OO   OO SS      
+	 MM MM MM AA   AA CC       OO   OO  SSSSS  
+	 MM    MM AAAAAAA CC    C  OO   OO      SS 
+	 MM    MM AA   AA  CCCCC    OOOO0   SSSSS  
+                                          
+  AAA   PPPPPP  PPPPPP   SSSSS  TTTTTTT  OOOOO  RRRRRR  EEEEEEE 
+ AAAAA  PP   PP PP   PP SS        TTT   OO   OO RR   RR EE      
+AA   AA PPPPPP  PPPPPP   SSSSS    TTT   OO   OO RRRRRR  EEEEE   
+AAAAAAA PP      PP           SS   TTT   OO   OO RR  RR  EE      
+AA   AA PP      PP       SSSSS    TTT    OOOO0  RR   RR EEEEEEE 
+                                                                
+  UU   UU PPPPPP  DDDDD     AAA   TTTTTTT EEEEEEE RRRRRR  
+  UU   UU PP   PP DD  DD   AAAAA    TTT   EE      RR   RR 
+  UU   UU PPPPPP  DD   DD AA   AA   TTT   EEEEE   RRRRRR  
+  UU   UU PP      DD   DD AAAAAAA   TTT   EE      RR  RR  
+   UUUUU  PP      DDDDDD  AA   AA   TTT   EEEEEEE RR   RR 
+                                                        
 """)
-	print("Version 1.3")
-	print("Created by eLVee")
-	print("-"*60)
+	message("underline", "Version 1.4")
+	message("underline", "Created by eLVee")
+	print("")
 
 def Install_Homebrew():
-	print("Running script for installing Homebrew for Mac")
+	message("succes", "[+] Running script for installing Homebrew for Mac")
 	print("")
-	command_2 = '''/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'''
-	subprocess.Popen(command_2, shell=True)
-	print("Done installing Homebrew")
-	print("-"*60)
-	print("")
+	install_homebrew = '''/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'''
+	command = pexpect.spawnu(install_homebrew)
+	command.expect("")
+	command.sendline()
+	message("succes", "[+] Done installing Homebrew")
 
 def Update_Homebrew():
-	print("Installing update of homebrew.")
-	subprocess.Popen("brew update", shell=True)
-	print("-"*60)
-	print("")
+	message("succes", "[+] Installing update of homebrew.")
+	command = pexpect.spawnu("brew update")
+	command.interact()
+	command.close()
+	message("succes", "[+] Done installing homebrew")
 
 def Install_Mas():
-	subprocess.Popen("brew install mas", shell=True)
-	print("MAS (Mac Apple Store) package installed.")
-	print("-"*60)
-	print("")
+	message("succes", "[+] Installing brew package 'MAS'")
+	pexpect.spawnu("brew install mas")
+	command.interact()
+	command.close()
+	message("succes", "[+] MAS (Mac Apple Store) installed.")
 
 def Update_AppleStore():
-	print("Running MAS (Mac Apple Store) updater")
-	print("Showing outdated apps.")
-	subprocess.Popen('mas outdated', shell=True)
-	print("Updating the apps now..")
-	subprocess.Popen('mas upgrade', shell=True)	
-	print("Done updating Apple Store apps")
-	print("-"*60)
-	print("")
+	message("succes", "[+] Running MAS (Mac Apple Store) updater")
+	print("Showing outdated apps:")
+	command = pexpect.spawnu('mas outdated')
+	command.interact()
+	command.close()
+	message("succes", "[+] Updating the apps now..")
+	command = pexpect.spawnu('mas upgrade')
+	command.interact()
+	command.close()
+	message("succes", "[+] Done updating Apple Store apps")
 
 if __name__ == "__main__":
+	Text()
+	message("warning", "[+] Before you can use this script you need to fill in your password below. This will make sure the script can update automatically.")
+	password = getpass.getpass()
+	if password != None:
+		message("succes", "[+] The script will continue to run now")
+	else:
+		message("warning", "No/bank password was entered..")
 	Main()
