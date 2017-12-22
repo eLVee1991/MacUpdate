accc204b2b2a10d3219882561f53ed4bbee976c5
eLVee1991 <elvee@protonmail.com>
2017-12-23 00:34:09 +0100

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,209 +1,109 @@
 from pexpect import pxssh
-import getpass
+import subprocess
 import sys
+from modules.messageColor import message
+from modules.inStaller import *
+from modules.upDater import *
+from modules.cryptor import ifExist
 
-connection_list = [
-"enter ip adressen here"
-]
 
-def Main():
-    usage = """
-usage: python3 controller.py [-i] [-u] [-g]
+"""
+- sshcontroler aanpassen naar een autoconnect via ssh (keychain onthouden en toevoegen aan servers CMD+K)
+als sshcontroler werkt, dan zorgen dat de keys.enc en gen.enc gehost staan op de sshserver en via ssh
+worden verstuurd, zodat daar alleen het wachtwoord staat. En nergerns anders. Tot slot cronjob van maken, zodat hij
+het automatisch verstuurd.
 
-optional arguments:
-  -i.   Run the install script of macupdate.py via SSH.
-  -u.   Run the update script of macupdate.py via SSH.
-  -g    Update the script from github via SSH.
+
+- IPV SSH kan je ook een server, en client aanmaken....
+
+
+- een optie argv. -e toevoegen om een encryptie bestand aan te maken, lostaant van het macupdate script.
 """
 
-    Text()
-    argument = sys.argv
-    try:
-        if argument[1] == "-i":
-            MacUpdate_Install()
-            exit()
-        elif argument[1] == "-u":
-            MacUpdate_Update()
-            exit()
-        elif argument[1] == "-g":
-            Update_Git()
-            exit()
-        elif argument[1] is not "-i" or "-u" or "-g":
-            print(usage)
-            exit()
+def Update_OS():
+	for connection in connection_list:
+		message("succes", "[+] Connecting to "+username+"@"+connection+" to update mac OS software")
+		child = pxssh.pxssh()
+		child.login(connection, username, password)
+		message("succes", "[+] Updating your mac. Please wait.")
+		child.sendline('sudo softwareupdate -ia --verbose')
+		child.expect('Password:')
+		child.sendline(password)
+		child.sendline()
+		message("succes", "[+] Updating mac OS software")
+		child.interact()
+		child.logout()
+		message("succes", "[+] Done updating mac OS software")
 
-    except IndexError:
-        print("-"*60)
-        print("Error: No 2nd argument added. See the usage below:")
-        print("-"*60)
-        print(usage)
-        print("-"*60)
-        while True:
-            mode = input("""
-Do you want to controll the script manually? y/n: """)
-            if mode == "y":
-                mode2 = input("""
-i.   Run the install script of macupdate.py via SSH.
-u.   Run the update script of macupdate.py via SSH.
-g    Update the script from github via SSH.
+def Main():
+	usage = """
+usage: python controller.py [-c] user ip [-i] or [-u] 
+example: python controller.py -c admin 192.168.0.1 -u
+or user the [-h] for this usage page.
+optional arguments:
+  -c.   Creates a connection to the given ip.
+  -i.   Install Homebrew and brew MAS (Mac Apple Store) so you can use the script.
+  -u.   Update Homebrew, update brew MAS, update OSX and update M.A.S.
+  -h.   Show this help message and exit
+"""
 
-enter here: """)
-                if mode2 =="i":
-                    MacUpdate_Install()
-                    bre
-                elif mode2 =="u":
-                    MacUpdate_Update()
-                    break
-                elif mode2 =="g":
-                    Update_Git()
-                    break
-                else:
-                    print("-"*60)
-                    print("Error: wrong input. Please enter i, u or g..")
-                    print("-"*60)
-                    exit()
-            if mode == "n":
-                exit()
+	arg = sys.argv
+	username = arg[2]
+	ip = arg[3]
+	try:
+		if arg[1] == "-c" and arg[4] == "-i":
+			message("succes", "[+] Connecting to "+username+"@"+ip+" to run macupdate.py script (script must be installed!)")
+			child = pxssh.pxssh()
+			child.login(connection, username, password)
+			message("succes", "[+] Updating your mac. Please wait.")
+			child.sendline("python macupdate.py -i")
+			exit()
+		elif arg[1] == "-c" and arg[4] == "-u"::
+			message("succes", "[+] Connecting to "+username+"@"+ip+" to run macupdate.py script (script must be installed!)")
+			child.login(connection, username, password)
+			message("succes", "[+] Updating your mac. Please wait.")
+			child.sendline("python macupdate.py -u")
+			exit()
+		elif arg[1] == "-h":
+			print(usage)
+			exit()
+		else:
+			message("warning", usage)
+			exit()
+
+	except IndexError:
+		print("-"*60)
+		message("error", "[+] No 2nd argument added. See the usage below:")
+		print("-"*60)
+		print("")
+		print(usage)
+		exit()
 
 def Text():
-    print("""
-          ╔═╗╔═╗╦ ╦           
-          ╚═╗╚═╗╠═╣           
-          ╚═╝╚═╝╩ ╩           
-╔═╗┌─┐┌┐┌┌┬┐┬─┐┌─┐┬  ┬  ┌─┐┬─┐
-║  │ ││││ │ ├┬┘│ ││  │  ├┤ ├┬┘
-╚═╝└─┘┘└┘ ┴ ┴└─└─┘┴─┘┴─┘└─┘┴└─
-                  
-       for macupdate.py
-             1.0                                 
+	message("info", """
+                        .dP"Y8 .dP"Y8 88  88                            
+                        `Ybo." `Ybo." 88  88                            
+                        o.`Y8b o.`Y8b 888888                            
+                        8bodP' 8bodP' 88  88
 
-""")
-    print("Created by eLVee")
-    print("")
-    print("-"*60)
-    print("see my github account:")
-    print("https://github.com/eLVee1991/MacUpdate")
+8b    d8    db     dP""b8 88   88 88""Yb 8888b.     db    888888 888888 
+88b  d88   dPYb   dP   `" 88   88 88__dP  8I  Yb   dPYb     88   88__   
+88YbdP88  dP__Yb  Yb      Y8   8P 88.     8I  dY  dP__Yb    88   88   
+88 YY 88 dP    Yb  YboodP `YbodP' 88     8888Y"  dP    Yb   88   888888 
 
-def Update_Git():
-    global process
-    print("-"*60)
-    print("Please enter the password to the SSH client here: ")
-    password = getpass.getpass('password: ')
-    print("-"*60)
-    try:
-        while True:
-            cloneorpull = input("""
-Want to clone or pull for github reposity? c/p: """)
-            if cloneorpull == "c":
-                for connection in connection_list:
-                    username = input("Fill in the username: ")
-                    s = pxssh.pxssh()
-                    s.login(connection, username, password)
-                    s.sendline('cd Desktop')   # run a command
-                    s.prompt()             # match the prompt
-                    print(s.before)        # print everything before the prompt.
-                    s.sendline('git clone https://github.com/eLVee1991/MacUpdate.git')
-                    s.prompt()
-                    print(s.before)
-                    s.sendline('cd MacUpdate && git init')
-                    s.prompt()
-                    print(s.before)
-                    s.logout()
-                    print("-"*60)
-                    print("Created a folder named MacUpdate on the Desktop. And made it a git repository.")
-                    print("-"*60)
-                print("Done with all connections.")
-                print("-"*60)
-                break
-            elif cloneorpull == "p":
-                for connection in connection_list:
-                    username = input("Fill in the username: ")
-                    s = pxssh.pxssh()
-                    s.login(connection, username, password)
-                    s.sendline('cd Desktop/MacUpdate')   # run a command
-                    s.prompt()             # match the prompt
-                    print(s.before)        # print everything before the prompt.
-                    s.sendline('git pull https://github.com/eLVee1991/MacUpdate.git')
-                    s.prompt()
-                    print(s.before)
-                    s.logout()
-                    print("-"*60)
-                    print("Pulled the newest script from the github reposity. Updated readme and macupdate.py")
-                    print("Done with "+connection)
-                    print("-"*60)
-                print("Done with all connections.")
-                print("-"*60)
-                break
-            else:
-                print("-"*60)
-                print("Error: please enter c or p)
-                print("-"*60)
-                
-    except pxssh.ExceptionPxssh as e:
-        print("-"*60)
-        print("pxssh failed on login.")
-        print(e)
-        print("-"*60)
+""")                                
+	message("underline", "Version 1.3")
+	message("underline", "Created by eLVee")
+	print("")
 
-def Stop():
-    # Uncomment this if you want the process to terminate along with the window
-    process.terminate()
-
-def MacUpdate_Install():
-    global process
-    print("-"*60)
-    print("Please enter the password to the SSH client here: ")
-    password = getpass.getpass('password: ')
-    print("-"*60)
-    try:
-        for connection in connection_list:
-            username = input("Fill in the username: ")
-            s = pxssh.pxssh()
-            s.login(connection, username, password)
-            s.sendline('cd Desktop/MacUpdate')   # run a command
-            s.prompt()             # match the prompt
-            print(s.before)        # print everything before the prompt.
-            s.sendline('python3 macupdate.py -i')
-            s.prompt()
-            print(s.before)
-            s.logout()
-            print("-"*60)
-            print("Done with "+connection)
-            print("-"*60)
-        print("Done with all connections.")
-        print("-"*60)
-    except pxssh.ExceptionPxssh as e:
-        print("-"*60)
-        print("pxssh failed on login.")
-        print(e)
-        print("-"*60)
-
-def MacUpdate_Update():
-    global process
-    print("-"*60)
-    print("Please enter the password to the SSH client here: ")
-    password = getpass.getpass('password: ')
-    print("-"*60)
-    try:
-        for connection in connection_list:
-            username = input("Fill in the username: ")
-            s = pxssh.pxssh()
-            s.login(connection, username, password)
-            s.sendline('cd Desktop/Macupdate')   # run a command
-            s.prompt()             # match the prompt
-            print(s.before)        # print everything before the prompt.
-            s.sendline('python3 macupdate.py -u')
-            s.prompt()
-            print(s.before)
-            s.logout()
-            print("-"*60)
-            print("Done with "+connection)
-            print("-"*60)
-        print("Done with all connections.")
-        print("-"*60)
-    except pxssh.ExceptionPxssh as e:
-        print("pxssh failed on login.")
-        print(e)
 
 if __name__ == "__main__":
-    Main()
+	Text()
+	fileExist = ifExist('logs/keys.enc', "logs/gen.enc")
+	if fileExist:
+		message("succes", "[+] The script will continue to run now")
+		Main()
+	else:
+		notExist()
+		message("info", "[+] Please run the script again.")
+		exit()
